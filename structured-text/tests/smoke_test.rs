9f80156b3c05f367067d29bcb5a0f5e555ed5ef8
Michael Bryan <michaelfbryan@gmail.com>
2019-11-24 15:23:28 +0800

    Added some more fixtures

--- structured-text/tests/smoke_test.rs
+++ structured-text/tests/smoke_test.rs
@@ -1,24 +1,29 @@
 /// A helper to generate a test that parses the contents of a "*.st" file in the
 /// "tests/data/" directory.
 macro_rules! parse_data_file {
-    ($( $( #[$attr:meta])* $name:ident, )*) => {
+    ($( $( #[$attr:meta] )* $name:ident => $parses_to:ident, )*) => {
         $(
-            parse_data_file!($(#[$attr])* $name);
+            #[test]
+            $( #[$attr] )*
+            fn $name() {
+                let src = include_str!(concat!("data/", stringify!($name), ".st"));
+
+                use rustmatic_structured_text::parser::{Parser, Rule};
+                use pest::Parser as _;
+
+                Parser::parse(Rule::$parses_to, src).unwrap();
+            }
         )*
-    };
-    ($( #[$attr:meta])* $name:ident) => {
-        #[test]
-        $( #[$attr] )*
-        fn $name() {
-            let src = include_str!(concat!("data/", stringify!($name), ".st"));
-            rustmatic_structured_text::parse(src).unwrap();
-        }
     };
 }
 
 parse_data_file! {
     #[ignore]
-    first_wikipedia_sample,
+    first_wikipedia_sample => program,
     #[ignore]
-    function_block,
+    function_block => function_block,
+    #[ignore]
+    if_start_while_initialized_prime_system => conditional,
+    #[ignore]
+    if_chain => conditional,
 }
