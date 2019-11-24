9021d2ee6574b0f3354d74ba2668efdf4630b56d
NOP0 <33583294+NOP0@users.noreply.github.com>
2019-11-24 20:17:33 +0100

    Removed my old stuff

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,3 @@
-pub mod plc;
 
 fn main() {
-    // bool example
-    let mut pi_bool: plc::ProcessImageInputs<bool> =
-        plc::ProcessImageInputs::new(1000);
-
-    let my_bool = pi_bool.insert(true);
-
-    println!("{:?}", pi_bool.get(my_bool).unwrap());
-
-    *(pi_bool.get_mut(my_bool).unwrap()) = false; // TODO: Poor ergonomics!
-
-    println!("{:?}", pi_bool.get(my_bool).unwrap());
-
-    // float example
-    let mut pi_float: plc::ProcessImageInputs<f64> =
-        plc::ProcessImageInputs::new(1000);
-
-    let my_float = pi_float.insert(10.0);
-
-    println!("{:?}", pi_float.get(my_float).unwrap());
-
-    *(pi_float.get_mut(my_float).unwrap()) = 13.0;
-
-    println!("{:?}", pi_float.get(my_float).unwrap());
 }
