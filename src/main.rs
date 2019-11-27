a251e91151525919fbf5438a088d105981e63156
NOP0 <33583294+NOP0@users.noreply.github.com>
2019-11-27 21:48:00 +0100

    Added example in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-
+use rustmatic_core::{Process, ProcessImage, System, Transition};
+use rustmatic_runtime::{Fault, Runtime};
 fn main() {
+    //Example usage
+
+    let mut runtime = Runtime::new();
+
+    struct PlcMain;
+
+    impl Process for PlcMain {
+        type Fault = Fault;
+
+        fn poll(
+            &mut self,
+            _system: &dyn System,
+            pi: &mut ProcessImage,
+        ) -> Transition<Self::Fault> {
+            println!("PlcMain running!");
+
+            let my_bool = pi.register_input(false);
+
+            println!("my_bool is:{}", pi.read::<bool>(my_bool));
+
+            pi.write(my_bool, true);
+
+            println!("my_bool is:{}", pi.read::<bool>(my_bool));
+
+            let my_float = pi.register_input(0.0);
+
+            println!("my_float is:{}", pi.read::<f64>(my_float));
+
+            pi.write(my_float, 3.14);
+
+            println!("my_float is:{}", pi.read::<f64>(my_float));
+
+            Transition::StillRunning
+        }
+    }
+
+    runtime.add_process(PlcMain);
+
+    let _ = runtime.poll();
 }
