0310565fc458c8df7fcabd4d3f0aecb2f307fc97
Michael Bryan <michaelfbryan@gmail.com>
2019-12-09 22:06:52 +0800

    Started adding more host bindings

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -47,6 +47,17 @@ pub enum Error {
     Other(#[source] Box<dyn std::error::Error>),
 }
 
+impl Error {
+    fn code(&self) -> i32 {
+        match self {
+            Error::AddressOutOfBounds => WASM_ADDRESS_OUT_OF_BOUNDS,
+            Error::UnknownVariable => WASM_UNKNOWN_VARIABLE,
+            Error::BadVariableType => WASM_BAD_VARIABLE_TYPE,
+            _ => WASM_GENERIC_ERROR,
+        }
+    }
+}
+
 impl From<CallError> for Error {
     fn from(other: CallError) -> Error { Error::Wasm(other) }
 }
@@ -85,6 +96,16 @@ impl Program {
         let imports = wasmer_runtime::imports! {
             "env" => {
                 "print" => wasmer_runtime::func!(print),
+                "wasm_log" => wasmer_runtime::func!(wasm_log),
+                "wasm_read_input" => wasmer_runtime::func!(wasm_read_input),
+                "wasm_write_output" => wasmer_runtime::func!(wasm_write_output),
+                "wasm_current_time" => wasmer_runtime::func!(wasm_current_time),
+                "wasm_variable_read_boolean" => wasmer_runtime::func!(wasm_variable_read_boolean),
+                "wasm_variable_read_double" => wasmer_runtime::func!(wasm_variable_read_double),
+                "wasm_variable_read_int" => wasmer_runtime::func!(wasm_variable_read_int),
+                "wasm_variable_write_boolean" => wasmer_runtime::func!(wasm_variable_write_boolean),
+                "wasm_variable_write_double" => wasmer_runtime::func!(wasm_variable_write_double),
+                "wasm_variable_write_int" => wasmer_runtime::func!(wasm_variable_write_int),
             },
         };
         let instance = wasmer_runtime::instantiate(wasm, &imports)?;
@@ -92,10 +113,64 @@ impl Program {
         Ok(Program { instance })
     }
 
-    pub fn poll(&mut self) -> Result<(), WasmerError> {
-        self.instance.call("poll", &[])?;
+    pub fn poll(&mut self, env: &mut dyn Environment) -> Result<(), Error> {
+        let mut state = State { env };
+        self.instance.context_mut().data = &mut state as *mut State as *mut _;
 
-        Ok(())
+        self.with_environment_context(env, |instance| {
+            instance.call("poll", &[])?;
+            Ok(())
+        })
+    }
+
+    fn with_environment_context<F, T>(
+        &mut self,
+        env: &mut dyn Environment,
+        func: F,
+    ) -> Result<T, Error>
+    where
+        F: FnOnce(&Instance) -> Result<T, Error>,
+    {
+        let mut state = State { env };
+        let instance = &mut self.instance;
+
+        // point the data pointer at our temporary state.
+        instance.context_mut().data = &mut state as *mut State<'_> as *mut _;
+        // we can't use the old state variable any more (we'd have aliased
+        // pointers) so deliberately shadow it
+        #[allow(unused_variables)]
+        let state = ();
+
+        // execute the callback. We need to catch panics so we can clear the
+        // data pointer no matter what. Using AssertUnwindSafe is
+        // correct here because we'll continue panicking once the data
+        // pointer is cleared
+        let got = panic::catch_unwind(AssertUnwindSafe(|| func(instance)));
+
+        // make sure the context data pointer is cleared. We don't need to drop
+        // anything because it was just a `&mut State
+        instance.context_mut().data = ptr::null_mut();
+
+        match got {
+            Ok(value) => value,
+            Err(e) => panic::resume_unwind(e),
+        }
+    }
+}
+
+/// Temporary state passed to each host function via [`Ctx::data`].
+struct State<'a> {
+    env: &'a mut dyn Environment,
+}
+
+impl<'a> State<'a> {
+    /// # Safety
+    ///
+    /// This assumes the [`Ctx`] was set up correctly using
+    /// [`Program::with_environment_context()`].
+    unsafe fn from_ctx(ctx: &mut Ctx) -> &'a mut State<'a> {
+        assert!(!ctx.data.is_null());
+        &mut *(ctx.data as *mut State)
     }
 }
 
@@ -110,6 +185,157 @@ fn print(ctx: &mut Ctx, msg: WasmPtr<u8, Array>, length: u32) -> i32 {
         },
         None => -1,
     }
+}
+
+const WASM_SUCCESS: i32 = 0;
+const WASM_GENERIC_ERROR: i32 = 1;
+const WASM_ADDRESS_OUT_OF_BOUNDS: i32 = 2;
+const WASM_UNKNOWN_VARIABLE: i32 = 3;
+const WASM_BAD_VARIABLE_TYPE: i32 = 4;
+
+/// Convenience macro for executing a method using the [`Environment`] pointer
+/// attached to [`Ctx::data`].
+///
+/// # Safety
+///
+/// See [`State::from_ctx()`] for the assumptions and invariants around safety.
+macro_rules! try_with_env {
+    ($ctx:expr, $method:ident ( $($arg:expr),* ), $failure_msg:expr) => {{
+        // the data pointer should have been set by `with_environment_context()`
+        if $ctx.data.is_null() {
+            return WASM_GENERIC_ERROR;
+        }
+
+        let state = State::from_ctx($ctx);
+
+        // call the method using the provided arguments
+        match state.env.$method( $( $arg ),* ) {
+            // happy path
+            Ok(value) => value,
+            Err(e) => {
+                // log the original error using the failure_msg
+                log::error!(concat!($failure_msg, ": {}"), e);
+
+                // then iterate through the causes and log those too
+                let mut cause = std::error::Error::source(&e);
+                while let Some(inner) = cause {
+                    log::error!("Caused by: {}", inner);
+                    cause = inner.source();
+                }
+
+                // the operation failed, return the corresponding error code
+                return e.code();
+            }
+        }
+    }};
+}
+
+macro_rules! wasm_deref {
+    (with $ctx:expr, * $ptr:ident = $value:expr) => {
+        match $ptr.deref($ctx.memory(0)) {
+            Some(cell) => cell.set($value),
+            None => return WASM_GENERIC_ERROR,
+        }
+    };
+}
+
+fn wasm_current_time(
+    ctx: &mut Ctx,
+    secs: WasmPtr<u64>,
+    nanos: WasmPtr<u32>,
+) -> i32 {
+    let elapsed = unsafe {
+        try_with_env!(ctx, elapsed(), "Unable to calculate the elapsed time")
+    };
+
+    wasm_deref!(with ctx, *secs = elapsed.as_secs());
+    wasm_deref!(with ctx, *nanos = elapsed.subsec_nanos());
+
+    WASM_SUCCESS
+}
+
+fn wasm_log(
+    ctx: &mut Ctx,
+    level: i32,
+    file: WasmPtr<u8, Array>,
+    file_len: i32,
+    line: i32,
+    message: WasmPtr<u8, Array>,
+    message_len: i32,
+) -> i32 {
+    unimplemented!()
+}
+
+fn wasm_read_input(
+    ctx: &mut Ctx,
+    address: u32,
+    buffer: WasmPtr<u8, Array>,
+    buffer_len: i32,
+) -> i32 {
+    unimplemented!()
+}
+
+fn wasm_write_output(
+    ctx: &mut Ctx,
+    address: u32,
+    data: WasmPtr<u8, Array>,
+    data_len: i32,
+) -> i32 {
+    unimplemented!()
+}
+
+fn wasm_variable_read_boolean(
+    ctx: &mut Ctx,
+    name: WasmPtr<u8, Array>,
+    name_len: i32,
+    value: WasmPtr<u8>,
+) -> i32 {
+    unimplemented!()
+}
+
+fn wasm_variable_write_boolean(
+    ctx: &mut Ctx,
+    name: WasmPtr<u8, Array>,
+    name_len: i32,
+    value: u8,
+) -> i32 {
+    unimplemented!()
+}
+
+fn wasm_variable_read_int(
+    ctx: &mut Ctx,
+    name: WasmPtr<u8, Array>,
+    name_len: i32,
+    value: WasmPtr<i32>,
+) -> i32 {
+    unimplemented!()
+}
+
+fn wasm_variable_write_int(
+    ctx: &mut Ctx,
+    name: WasmPtr<u8, Array>,
+    name_len: i32,
+    value: i32,
+) -> i32 {
+    unimplemented!()
+}
+
+fn wasm_variable_read_double(
+    ctx: &mut Ctx,
+    name: WasmPtr<u8, Array>,
+    name_len: i32,
+    value: WasmPtr<f64>,
+) -> i32 {
+    unimplemented!()
+}
+
+fn wasm_variable_write_double(
+    ctx: &mut Ctx,
+    name: WasmPtr<u8, Array>,
+    name_len: i32,
+    value: f64,
+) -> i32 {
+    unimplemented!()
 }
 
 pub struct InMemory {
