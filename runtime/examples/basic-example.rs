c206d142ac6b5b7f452e3553a5b4590d9912c3cf
NOP0 <33583294+NOP0@users.noreply.github.com>
2019-12-12 21:26:39 +0100

    Moved runtime example to examples folder

---
+++ runtime/examples/basic-example.rs
@@ -0,0 +1,98 @@
+use rustmatic_core::{AccessType, Process, System, Transition};
+use rustmatic_dummy_input::{DummyBool, DummyDoubleWord};
+use rustmatic_runtime::{Fault, Runtime};
+use std::sync::Arc;
+
+struct PlcMain {
+    cycle_counter: u64,
+    my_bool: bool,
+    my_double_word: u32,
+}
+
+impl PlcMain {
+    pub fn new(runtime: &mut Runtime) -> Self {
+        // DummyBool is a faked input device generating signal changes
+        let my_bool_input = DummyBool::new();
+
+        // Register this input at offset %I4.0 in input Process Image
+        runtime.inputs.register_input_device(
+            4,
+            0,
+            AccessType::Bit,
+            runtime.devices.register(Arc::new(my_bool_input)),
+        );
+
+        // DummyDoubleWord is a faked input device generating signal changes
+        let my_double_word_input = DummyDoubleWord::new();
+
+        // Register this input at offset %ID8 in input Process Image
+        runtime.inputs.register_input_device(
+            8,
+            0,
+            AccessType::DoubleWord,
+            runtime.devices.register(Arc::new(my_double_word_input)),
+        );
+
+        PlcMain {
+            cycle_counter: 0,
+            my_bool: false,
+            my_double_word: 0,
+        }
+    }
+}
+
+impl Process for PlcMain {
+    type Fault = Fault;
+
+    fn init(&mut self, _system: &mut dyn System) -> Result<(), Self::Fault> {
+        // Do some initializing
+        println!("PlcMain was initialized");
+        Ok(())
+    }
+
+    fn poll(&mut self, system: &mut dyn System) -> Transition<Self::Fault> {
+        println!("PlcMain running cycle #{}", self.cycle_counter);
+
+        // Read all inputs
+        {
+            let inputs = system.inputs();
+
+            self.my_bool = inputs.read_bit(4, 0);
+            self.my_double_word = inputs.read_double_word(8);
+        }
+
+        // Do something with them.
+        println!("The value of my_bool is: {}", self.my_bool);
+        println!("The value of my_double_word is: {}", self.my_double_word);
+
+        // Write all outputs
+        let outputs = system.outputs();
+
+        // Write to offset %Q0.0 in output Process Image
+        outputs.write_bit(0, 0, self.cycle_counter % 2 != 0);
+
+        println!("The value of output is: {}", outputs.read_bit(0, 0));
+
+        self.cycle_counter += 1;
+
+        if self.cycle_counter < 64000 {
+            Transition::StillRunning
+        } else {
+            println! {"64000 cycles completed."}
+            Transition::Completed
+        }
+    }
+}
+fn main() {
+    let mut runtime = Runtime::new();
+
+    let plc_main = PlcMain::new(&mut runtime);
+
+    runtime.add_process(plc_main);
+
+    runtime.init().expect("Could not init runtime");
+
+    while runtime.iter_processes().count() > 0 {
+        let _ = runtime.poll();
+    }
+}
